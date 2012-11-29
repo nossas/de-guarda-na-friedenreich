6f5e7187b87b64a6ab45a88622e87fafebf16ec2
Nícolas Iensen <nicolas@engage.is>
2012-11-28 23:11:35 -0200

    Add automatic share after sign in with Facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,11 @@ class SessionsController < ApplicationController
     if auth_hash
       hash = auth_hash 
       self.current_user = User.find_or_create_by_uid(hash[:uid], email: hash[:info][:email], name: hash[:info][:name], avatar: hash[:info][:image], mobile_phone: session[:phone])
+      Koala::Facebook::API.new(hash[:credentials][:token]).put_connections("me", "links", :link => "http://deguarda.meurio.org.br/", :message => "Acabo de me tornar um guardião da Friedenreich: Seja um você também!")
     else
       self.current_user = User.find_or_create_by_email(params[:user])
     end
 
-    Koala::Facebook::API.new(hash[:credentials][:token]).put_connections("me", "links", :link => "http://deguarda.meurio.org.br/", :message => "Acabo de me tornar um guardião da Friedenreich: Seja um você também!")
-      
     return redirect_to root_path
   end
 
