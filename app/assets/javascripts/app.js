b0f7a94b1ba0d6fb1ce42ac7570711216a7bfddc
Luiz Fonseca <runeroniek@gmail.com>
2012-11-28 23:39:53 -0200

    Try to fix facebook share.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,7 +4,7 @@ var initializeSocial = function(){
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=392681980810591";
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=301170913317565";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
