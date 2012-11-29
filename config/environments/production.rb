7d3e693ef99fd1e568ca06847f76b669945ed55f
Luiz Fonseca <runeroniek@gmail.com>
2012-11-29 01:10:57 -0200

    Change notify's method name.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Brickonthewall::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   #
   #
-    ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
